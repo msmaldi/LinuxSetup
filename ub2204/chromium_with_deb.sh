43a8f349a3f1d956e93b5733a4e3b788ff942cda
Matheus Maldi <msmaldi@hotmail.com>
2023-02-09 07:29:39 -0300

    Update Chromium as deb

--- ub2204/chromium_with_deb.sh
+++ ub2204/chromium_with_deb.sh
@@ -11,14 +11,9 @@ deb http://archive.ubuntu.com/ubuntu/ bionic-updates universe
 EOF
 
 cat << EOF > /etc/apt/preferences.d/chromium-deb-bionic-updates
-Package: chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg-extra chromium-codecs-ffmpeg chromium-chromedriver
+Package: chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg-extra chromium-codecs-ffmpeg 
 Pin: release a=bionic-updates
 Pin-Priority: 900
-
-Package: chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg-extra chromium-codecs-ffmpeg chromium-chromedriver
-Pin: release a=kinetic
-Pin-Priority: -10
-
 EOF
 
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
